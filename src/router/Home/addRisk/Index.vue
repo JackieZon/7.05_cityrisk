9e057e2a107372c0e3536d1d8f22fa3581d6a866
JackieZon <mrjackie168@gmail.com>
2017-07-11 19:53:49 +0800

    updata

--- src/router/Home/addRisk/Index.vue
+++ src/router/Home/addRisk/Index.vue
@@ -1,18 +1,39 @@
 <template>
     <div>
         <Heads :back="false" :title="'新增风险源'"></Heads>
-        我是新增风险页
+        <div>
+            <tab :line-width=2 active-color='#33CC99' v-model="index">
+                <tab-item class="vux-center" :selected="status === item" v-for="(item, index) in lists" @click="status = item" :key="index">{{item}}</tab-item>
+            </tab>
+            <swiper v-model="index" height="100px" :show-dots="false">
+                <swiper-item v-for="(item, index) in lists" :key="index">
+                    <router-view></router-view>
+                </swiper-item>
+            </swiper>
+        </div>
     </div>
 </template>
 <script>
     import Heads from './../../../components/Heads.vue'
+    import { Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem } from 'vux'
+    const list = () => ['基本信息', '评估信息', '责任主体', '监管机构']
+
     export default{
         components:{
-            Heads
+            Heads,
+            Tab,
+            TabItem,
+            Sticky,
+            Divider,
+            XButton,
+            Swiper,
+            SwiperItem,
         },
         data(){
             return{
-
+                lists: list(),
+                index: 0,
+                status:'基本信息',
             }
         },
     }
